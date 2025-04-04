a4cec7b3b4fe2a53ebf205274a5b6e2c9e88fc13
mrkmartin <martinmarkpaulo12@gmail.com>
2025-04-04 20:19:36 +0800

    Added button animation and edited readme

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 "use strict";
 
 const btn = document.querySelector(".button-container");
+const dice = document.querySelector(".button-container img");
 const adviceContainer = document.querySelector(".advice");
 const adviceId = document.querySelector("#advice-number");
 const adviceText = document.querySelector("#advice-text");
 
 const getAdvice = async () => {
+  dice.classList.remove("roll-dice");
+  void dice.offsetWidth;
+  dice.classList.add("roll-dice");
+
   try {
     const response = await fetch("https://api.adviceslip.com/advice");
     if (!response.ok) {
